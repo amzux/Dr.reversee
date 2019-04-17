cf52a38375a4399ddbcd596f8cc9c7c89c079b05
Amzker <amzker@protonmail.com>
2019-04-17 08:39:37 +0000

    Update Dr.Reversee.sh Try to manage bumblebee

--- Dr.Reversee.sh
+++ Dr.Reversee.sh
@@ -3,6 +3,7 @@ n1="1"
 n2="2"
 n3="3"
 n4="4"
+n5="5"
 y="Y"
 yy="y"
 echo "+====================+"
@@ -13,7 +14,7 @@ echo "[1] Messed With Repo"
 echo "[2] Could Not Resolve Repo"
 echo "[3] NO GUI after Update "
 echo "[4] Safe Update"
-
+echo "[5] Install Bumblebee"
 read qas
 
 
@@ -27,18 +28,18 @@ sudo apt update | echo "Failed! Please check Internet:)) || select option 2"
 fi ;
 
 	if [ $qas = $n2 ] ; then
-	sudo anonsurf start && sudo parrot-upgrade
+	sudo anonsurf start && sudo parrot-upgrade && sudo parrot-upgrade && sudo parrot-upgrade
         		echo "updated or still error? [Y/N]"
 			read  qad
 				 if [ $qad = $y ] ; then
 				 echo "Change _apt:x:100:65534 to _apt:x:0:65534";sleep 2
 				 sudo nano /etc/passwd
-				 sudo parrot-upgrade
+				 sudo parrot-upgrade && sudo parrot-upgrade && sudo parrot-upgrade
 	                 	 fi ;
 					if [ $qad = $yy ] ; then
 					echo "Change _apt:x:100:65534 to _apt:x:0:65534";sleep 2
                                         sudo nano /etc/passwd
-                                        sudo parrot-upgrade
+                                        sudo parrot-upgrade && sudo parrot-upgrade
 					else
 					echo "Cool"
 					fi ;
@@ -49,7 +50,7 @@ fi ;
 if [ $qas = $n3 ] ; then
 clear;sudo apt remove --purge xorg lightdm parrot-interface parrot-lxde
 sudo apt install xorg sddm parrot-lxde parrot-interface
-service xorg startc | service lightdm start
+service xorg start | service lightdm start | startxfce 
 fi ;
 
 if [ $qas = $n4 ] ; then
@@ -57,5 +58,10 @@ clear;sudo rm -r /etc/apt/sources.list
 sudo rm -r /etc/apt/sources.list.d/*
 sudo touch /etc/apt/sources.list
 sudo echo deb http://deb.parrotsec.org/parrot stable main contrib non-free>>/etc/apt/sources.list.d/parrot.list
-sudo parrot-upgrade
+sudo parrot-upgrade && sudo parrot-upgrade && sudo parrot-upgrade 
 fi ;
+
+  if [ $qas = $n5 ] ; then 
+  sudo apt update && bumblebee-nvidi* primus libg*1-nvidia-g*x
+  clear && echo "Make sure you check"
+  fi ; 
